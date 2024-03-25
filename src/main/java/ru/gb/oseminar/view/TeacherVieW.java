2d889aa2d6d0c5d1f3ea778f19edd3b319064790
Oleg <schika145@gmail.com>
2024-03-25 15:21:59 +1000

    Подправленый DZ4

--- src/main/java/ru/gb/oseminar/view/TeacherVieW.java
+++ src/main/java/ru/gb/oseminar/view/TeacherVieW.java
@@ -7,8 +7,8 @@ public class TeacherVieW <T extends User> {
     Logger logger = Logger.getLogger(TeacherVieW.class.getName());
 
     @Override
-    public void sendOnConsole(List<Teacher> teachers) {
-        for(Teacher user: teachers){
+    public void sendOnConsole(List<T> teachers) {
+        for(T user: teachers){
             logger.info(user.toString());
         }
     }
